af2833e5ae51b28ebc9861ab0c971997d4d5bbba
satishsingh21 <34952664+satishsingh21@users.noreply.github.com>
2019-01-02 15:21:01 +0530

    Update authConfig.js

--- server/config/authConfig.js
+++ server/config/authConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     googleClient: {
-        clientID: '836125867855-qiskkvas5jv8l2723h996lhlqhnt61i0.apps.googleusercontent.com',
-        clientSecret: '1vSXsw6o-KYgLITwCHHGJD2n'
+        clientID: '**********************.apps.googleusercontent.com',
+        clientSecret: '******************'
     },
     appSecret: 'useRealSecret'
 };
